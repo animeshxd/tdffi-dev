d73140a704d0120f2f9f06523edde4bd11cd6952
Animesh Murmu <am2646374@gmail.com>
2023-01-15 20:02:08 +0530

    first commit

---
+++ pass_1.py
@@ -0,0 +1,161 @@
+import json
+import logging
+from pprint import pprint
+import re
+
+from utils import CamelCase, Serilizer, process_tl_parameter
+
+
+TL_API_FILE = "./td_api.tl"
+
+parameter_descriptions = {}
+abc_classes = {}
+classes = {}
+functions = {}
+
+def reset():
+    # print(parameter_descriptions)
+    parameter_descriptions.clear()
+    # input()
+    ...
+
+
+def process_comment(raw_comment: str):
+    _comment = None
+    for r in re.finditer(r"(?=(@(param_)?(\w+)(.+?))(@|$))", raw_comment.strip(), re.M):
+        parameter, description = r.group(3), r.group(4) # variable # description
+        parameter_ = r.group(2) # param_
+        if parameter == 'description' and parameter_ is None:
+            # print(description)
+            _comment = description
+        else:
+            parameter_descriptions[parameter] = {
+                "description": description.strip(),
+                "nullable": bool(re.search("(?i)(may be null|If empty|pass null)", description))
+            }
+    return _comment.strip()
+
+def process_body(raw_body: str):
+
+    class_body, parent = raw_body.strip()[:-1].split("=") # ignore ';' and split
+    class_body = class_body.split()
+    # print(class_body)
+
+    class_name = CamelCase(class_body[0])
+    class_param = map(lambda a: process_tl_parameter(a, parameter_descriptions), class_body[1:])
+    list(class_param)
+    return class_name, parameter_descriptions, parent.strip()
+
+def put_abc(key, value = None, description = None):
+    data = abc_classes.get(key.strip(), None)
+    assert value or description
+    if not data:
+        data  = {
+            "description": description,
+            "child": set()
+        }
+        if value:
+            data["child"].add(value.strip())
+        abc_classes[key.strip()] = data
+        return
+    data["child"].add(value.strip())
+
+def generate_json():
+    with open(TL_API_FILE) as f:
+        miss = True
+        is_function = False
+        regex = re.compile(r"(\/\/@class (.+) @description (.+))|(\/\/@description(.|\n)+?)(\n.* = .*;)$", re.M)
+        iterable = regex.finditer(f.read())
+        _class_or_func = classes
+        for i in iterable:
+            # assert len(i.groups()) == 3, (i.groups(), len(i.groups()))
+            _, abc, abc_desc, raw_comment, _, raw_body = i.groups()
+        #   1, 2,      3,     4,     5,    6, 7
+
+            # assert not abc and not abc_desc
+            if abc and abc_desc: # abstract class
+                # print(abc, abc_desc, sep=" : ")
+                put_abc(abc, description=abc_desc)
+                # continue
+            else:
+                
+                if raw_body == "\ngetAuthorizationState = AuthorizationState;": # now function starts
+                    is_function = True
+                    _class_or_func = functions
+                comment = process_comment(raw_comment)
+                class_name, class_body, parent = process_body(raw_body)
+                
+                _data = {
+                        "comment": comment,
+                        "parameters": class_body.copy(),
+                        "is-function": is_function,
+                        "return": ""
+                    }
+                if is_function:
+                    _data["return"] = parent
+                
+                # print(_data)
+                _class_or_func[class_name] = _data
+                if not is_function and parent.lower() != class_name.lower():
+
+                    put_abc(parent, class_name)
+                
+                # print()
+                # print(comment)
+                # print(class_name, class_body, sep=" : ")
+                # print()
+                reset()
+    json.dump(classes, open("classes.json", "w"), cls=Serilizer, indent=4)
+    json.dump(abc_classes, open("abstract-classes.json", "w"), cls=Serilizer, indent=4)
+    json.dump(functions, open("functions.json", "w"), cls=Serilizer, indent=4)
+
+if __name__ == "__main__":
+    generate_json()
+
+"""
+    for i in f:
+        if miss:
+            if i.startswith('vector'):
+                miss = False
+            continue  
+        if i.startswith('---functions---'): # Now function starts
+            break
+
+        if i.startswith("//"):
+            reset_count = 0
+        
+        if i.startswith('\n'): # Reset
+            reset()
+                    
+
+        
+        if i.startswith('//@class'): # Now the class is abstract
+            # print(i)
+            ...
+        
+        # CLASS
+        if i.startswith("//") and not i.startswith('//@class'): # comment / description
+            for r in re.finditer(r"(?=(@(param_)?(\w+)(.+?))(@|$))", i):
+                parameter, description = r.group(3), r.group(4) # variable # description
+                parameter_ = r.group(2) # param_
+                if parameter == 'description' and parameter_ is None:
+                    print(description)
+                else:
+                    parameter_descriptions[parameter] = {
+                        "description": description.strip(),
+                        "nullable": bool(re.search("(?i)(may be null|If empty|pass null)", description))
+                    }
+        if not i.startswith(('//', '\n')): # Class Defination
+            class_body, parent = i[:-1].split("=") # ignore ';' and split
+            class_body = class_body.split()
+            # print(class_body)
+
+            class_name = CamelCase(class_body[0])
+            class_param = map(lambda a: process_tl_parameter(a, parameter_descriptions), class_body[1:])
+            list(class_param)
+
+
+        # ENDS CLASS
+        
+
+"""
---
+++ utils.py
@@ -0,0 +1,81 @@
+
+import enum
+import json
+import re
+from typing import Any, Optional, Tuple
+
+
+class Serilizer(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, set):
+            return list(o)
+        if isinstance(o, Type):
+            return o.value
+        return super().default(o)
+
+class Type(enum.Enum):
+    TL = 2
+    DART = 3
+    VECTOR_TL = 4
+    VECTOR_DART = 6
+
+
+dart_types = {
+    'double': 'double',
+    'string': 'String',
+    'int32': 'int',
+    'int53': 'int',
+    'int64': 'String',
+    'bytes': 'String', # 'Uint8List',
+    'Bool': 'bool',
+    "emojis": 'List<String>',
+    'dynamic': 'dynamic',
+
+    # vector {t:Type} # [ t ] = Vector t
+}
+
+
+
+def CamelCase(x: str):
+    return (x[0].upper()+x[1:]).strip()
+
+
+def get_dart_type(type_: str) -> Tuple[str, str, Type]:
+                                #    d     t   enum
+    
+    if type_.startswith('vector'):
+        return _vector_to_List(type_)
+
+    dart_type = dart_types.get(type_, None)
+
+    if dart_type is not None:
+        return dart_type, type_, Type.DART
+    else:
+        type_ = CamelCase(type_)
+        return type_, type_, Type.TL
+
+def _vector_to_List(vector):
+    type_ = re.search("vector<(\w+)>", vector).group(1)
+    dtype, _, istl = get_dart_type(type_)
+
+    return f"List<{dtype}>", _, Type(istl.value * 2)
+
+
+def get_tl_to_dart(tl: str):
+    parameter, type = tl.split(":")
+    type, tl, enum = get_dart_type(type)
+    data = {
+        "type": type,
+        "tl": tl,
+        "enum": enum
+    }
+    return parameter, data
+
+def process_tl_parameter(source: str, lookup_dict: dict):
+    parameter, data = get_tl_to_dart(source)
+    result: Optional[dict] = lookup_dict.get(parameter, None)
+    if not result:
+        raise KeyError(f"parameter: {parameter} is not available in lookup_dict")
+    result: dict
+    result.update(data)
+    return {parameter: result}
